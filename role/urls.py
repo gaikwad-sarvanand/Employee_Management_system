15c772009e5eb76f11d21c7f5603d4a85e2b4f37
gaikwad-sarvanand <gaikwadsarvanand123@gmail.com>
2022-12-20 11:21:57 +0530

    completed role app with crud operations

--- role/urls.py
+++ role/urls.py
@@ -4,5 +4,8 @@ from . import views as v
 app_name = "role"
 
 urlpatterns = [
-
+    path('add_role',v.add_role,name="add-role"),
+    path('list_role',v.list_role,name="list-role"),
+    path('<int:pk>/update/',v.update_role,name="update-role"),
+    path('<int:pk>/delete/',v.delete_role,name="delete-role"),
 ]
--- role/views.py
+++ role/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from.forms import Role,RoleForm
 
-# Create your views here.
+
+def add_role(request):
+    if request.method == 'POST':
+        form = RoleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("employee:index")
+    else:
+        form = RoleForm()
+        context = {'form':form}
+        return render(request,"add_role.html",context)
+    
+    
+def list_role(request):
+    roles = Role.objects.all()
+    context = {"roles":roles}
+    return render(request,"list_role.html",context)
+
+
+def update_role(request, pk):
+    role = Role.objects.get(id = pk) 
+    if request.method == 'POST':
+        form = RoleForm(request.POST, instance =role)
+        if form.is_valid():
+            form.save()
+            return redirect("role:list-role")
+    else:
+        form = RoleForm(instance=role)
+        context = {'form':form}
+        return render(request,"update_role.html",context)
+    
+def delete_role(request,pk):
+    role = Role.objects.get(id=pk)
+    role.delete()
+    return redirect("role:list-role")
